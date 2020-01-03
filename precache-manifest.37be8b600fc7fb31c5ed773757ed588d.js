727fba24482a2efd9c317a33809c84b65e75102b
yoyota <yoyota@users.noreply.github.com>
2020-01-03 12:16:20 +0000

    Automated deployment: Fri Jan  3 12:16:20 UTC 2020 7e9800449a43f10dd0956084e700e279649169ee

---
+++ precache-manifest.37be8b600fc7fb31c5ed773757ed588d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c40297fba3cdc397e1395b3e24b99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55ff7eef34d8b5847003",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1bcc37be0152209858ae",
+    "url": "/static/js/2.3fe651ef.chunk.js"
+  },
+  {
+    "revision": "f7a5a7976d1ff3d674fc5eb50547b572",
+    "url": "/static/js/2.3fe651ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55ff7eef34d8b5847003",
+    "url": "/static/js/main.bb0f3c8b.chunk.js"
+  },
+  {
+    "revision": "9c05a0b6aebde2cb7377",
+    "url": "/static/js/runtime-main.f23432bf.js"
+  }
+]);
