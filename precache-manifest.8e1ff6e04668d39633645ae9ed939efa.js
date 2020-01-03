f39af04c2ccc1997d27e75b648f0c59ed1d61678
yoyota <yoyota@users.noreply.github.com>
2020-01-03 12:50:06 +0000

    Automated deployment: Fri Jan  3 12:50:06 UTC 2020 1e7fd3adde02965a5e58e917a9153322384c64ed

--- precache-manifest.8e1ff6e04668d39633645ae9ed939efa.js
+++ precache-manifest.8e1ff6e04668d39633645ae9ed939efa.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81c40297fba3cdc397e1395b3e24b99d",
+    "revision": "10fecefcc5ab12838ee9bd31904e67e5",
     "url": "/index.html"
   },
   {
@@ -8,12 +8,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "1bcc37be0152209858ae",
-    "url": "/static/js/2.3fe651ef.chunk.js"
+    "revision": "0b73231970de8719d0c6",
+    "url": "/static/js/2.fa5fe3e4.chunk.js"
   },
   {
     "revision": "f7a5a7976d1ff3d674fc5eb50547b572",
-    "url": "/static/js/2.3fe651ef.chunk.js.LICENSE"
+    "url": "/static/js/2.fa5fe3e4.chunk.js.LICENSE"
   },
   {
     "revision": "55ff7eef34d8b5847003",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37be8b600fc7fb31c5ed773757ed588d.js"
+  "/precache-manifest.8e1ff6e04668d39633645ae9ed939efa.js"
 );
 
 self.addEventListener('message', (event) => {
